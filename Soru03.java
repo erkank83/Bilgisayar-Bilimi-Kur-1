4779bddfc0e231277d7e44eb39d8da18a51427e1
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-10-02 12:16:32 +0300

    19-üçgen oluşur mu

---
+++ Soru03.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Soru03 {
+
+    public static boolean ucgenOlusurMu(float a,float b,float c){
+
+        boolean sart1=((Math.abs(b-c) < a)&&(a < b+c));
+        boolean sart2=((Math.abs(a-c) < b)&&(b < a+c));
+        boolean sart3=((Math.abs(a-b) < c)&&(c < a+b));
+
+        if (sart1 && sart2 && sart3)return true;
+        else return false;
+    }
+    public static void main(String[] args) {
+
+        Scanner oku=new Scanner(System.in);
+        float a,b,c;
+        boolean durum;
+        System.out.print("a kenar uzunluğu :");
+        a=oku.nextFloat();
+        System.out.print("b kenar uzunluğu :");
+        b=oku.nextFloat();
+        System.out.print("c kenar uzunluğu :");
+        c=oku.nextFloat();
+        durum=ucgenOlusurMu(a,b,c);
+        if (durum==true){
+            System.out.println("Üçgen oluşur.");
+        }else {
+            System.out.println("Üçgen oluşmaz.");
+        }
+
+    }
+
+}
