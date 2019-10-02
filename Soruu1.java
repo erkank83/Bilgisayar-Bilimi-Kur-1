c9c0b76703fa6dd3ff657d9112c72d963542d10e
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-10-02 14:17:54 +0300

    2- Merhaba İsim ilkel çözüm

---
+++ Soruu1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Soruu1 {
+
+    public static void main(String[] args) {
+        Scanner oku=new Scanner(System.in);
+        System.out.print("İsminizi giriniz: ");
+        String isim=oku.next();
+        System.out.println("Merhaba "+isim );
+    }
+
+
+}
