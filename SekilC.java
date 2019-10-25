f11758fa1d2da2aae05b6c877ec69ff8317aad15
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-10-25 09:09:34 +0300

    27 - Şekilleri ekrana yazdırma

---
+++ SekilC.java
@@ -0,0 +1,11 @@
+package tr.k12.meb.ibrahimonalfenlisesi;
+
+public class SekilC {
+    public static void main(String[] args) {
+        System.out.printf("%1$6s%n", " ********** ");
+        System.out.printf("%1$6s%n", " ********** ");
+        System.out.printf("%1$6s%n", " ********** ");
+        System.out.printf("%1$6s%n", " ********** ");
+        System.out.printf("%1$6s%n", " ********** ");
+    }
+}
