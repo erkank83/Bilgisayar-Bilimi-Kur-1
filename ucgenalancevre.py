6607fa93644d12923a2b2c2b8cddffa1d8b45457
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-10-30 12:01:24 +0300

    29 - Dik üçgen alan ve çevre hesabı

---
+++ ucgenalancevre.py
@@ -0,0 +1,6 @@
+a=input("Kenar uzunluğu :")#klavyeden kenar uzunluğu giriliyor
+h=input("Yükseklik      :")#klavyeden yükseklik giriliyor
+a=int(a)                   #str tipi int tipine dönüşüyor
+h=int(h)                   #str tipi int tipine dönüşüyor
+alan=a*h/2                 #alan hesaplanıyor
+print("Alan=",alan)        #Alan= 10.0
