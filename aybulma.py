95a119d8219a287e130a0b9d8628bcb5f3728adc
erkank83 <55956112+erkank83@users.noreply.github.com>
2019-12-25 09:18:29 +0300

    ay bulma python ilkel kod

---
+++ aybulma.py
@@ -0,0 +1,26 @@
+ay=int(input("Ay bilgisi giriniz(1-12):"))
+if ay ==1:
+    hangiAy="Ocak"
+elif ay==2:
+    hangiAy="Şubat"
+elif ay==3:
+    hangiAy="Mart"
+elif ay==4:
+    hangiAy="Nisan"
+elif ay==5:
+    hangiAy="Mayıs"
+elif ay==6:
+    hangiAy="Haziran"
+elif ay==7:
+    hangiAy="Temmuz"
+elif ay==8:
+    hangiAy="Ağustos"
+elif ay==9:
+    hangiAy="Eylül"
+elif ay==10:
+    hangiAy="Ekim"
+elif ay==11:
+    hangiAy="Kasım"
+elif ay==12:
+    hangiAy="Aralık"
+print("Girmiş olduğunuz ay:",hangiAy)
