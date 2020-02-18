8a3e1267a8e77a4b378ff00fb4a66bfb7d26e925
erkank83 <55956112+erkank83@users.noreply.github.com>
2020-02-18 10:54:02 +0300

    dikdörtgen prizma python parametreli

--- dikdortgenprizma.py
+++ dikdortgenprizma.py
@@ -1,6 +1,18 @@
-a=input("a kenar uzunluğu :")
-b=input("b kenar uzunluğu :")
-c=input("c kenar uzunluğu :")
-hacim=int(a)*int(b)*int(c)
-yuzeyAlan=2*(int(a)*int(b)+int(a)*int(c)+int(b)*int(c))
-print("Hacim: "+str(hacim)+"  Yüzey Alan: "+str(yuzeyAlan))
+#hesaplaHacim yardımcı fonksiyon
+def hesaplaHacim(a,b,c):
+    hacim = a*b*c
+    return hacim
+#hesaplaYuzeyAlan yardımcı fonksiyon
+def hesaplaYuzeyAlan(a,b,c):
+    alan = 2*(a*b+a*c+b*c)
+    return alan
+#Ana fonksiyon tanımlanır
+a=float(input("a kenar uzunluğu :"))
+b=float(input("b kenar uzunluğu :"))
+c=float(input("c kenar uzunluğu :"))
+alan=hesaplaYuzeyAlan(a,b,c)
+hacim=hesaplaHacim(a,b,c)
+print("A = 2 .( ab + ac +bc)")
+print("A = {}".format(round(alan,2)))
+print("V = a.b.c ")
+print("V = {}".format(round(hacim,2)))
